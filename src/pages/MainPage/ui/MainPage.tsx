677d220622d94a67291188289ab3f19f4614a383
AleksARPV <ascanalex@gmail.com>
2023-02-23 11:28:42 +0300

    delete Counter component from MainPage

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { Counter } from 'entities/Counter'
 
 const MainPage = () => {
     const { t } = useTranslation('mainpage')
     return (
         <div>
             {t('Main Page')}
-            <Counter/>
         </div>
     )
 }
